30b5c41088bfb23da744e28c408efee3ddba3b94
Marcos Paulo <codemarkpaul@gmail.com>
2025-04-03 23:14:14 -0300

    feat: adiciona limites de transação e mostra data e hora no estrato

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 """Sistema bancário simples"""
 
-#  Operação de depósito
+from datetime import datetime
+
+#  v1.0
+# ~ TODO: Operação de depósito
 # ~ Deve ser possível depositar valores positivos para a conta bancária.
 # ~ A v1 do projeto trabalha apenas com 1 usuário, dessa forma não é preciso se
 # ~ preocupar em identificar qual o número da conta ou da agência.
 # ~ Todos os depósitos devem ser armazenados em uma variável e exibidos na
 # ~ operação de extrato.
 
-#  Operação de saque
+# ~ TODO: Operação de saque
 # ~ O sistema deve permitir realizar 3 saques diários com limite máximo de
 # ~ R$ 500,00 por saque.
 # ~ Caso o usuário não tenha saldo suficiente na conta, o sistema deve exibir
@@ -15,11 +18,20 @@
 # ~ Todos os saques devem ser armazenados em uma variável e exibidos na
 # ~ operação de extrato.
 
-#  Operação de extrato
+# ~ TODO: Operação de extrato
 # ~ Essa operação deve listar todos os depósitos e saques realizados na conta.
 # ~ No fim da listagem deve ser exibido o saldo atual da conta.
 # ~ Os valores devem ser exibidos usando o formato R$ XXX.XX, exemplo:
 # ~ 1500.45 = R$ 1500.45
+
+
+#  v2.0
+# ~ TODO: Recursos com data e hora.
+# ~ Estabelecer um limite de 10 transações diárias (manter o limite de saques).
+# ~ Se o usuário tentar fazer uma transação após atingir o limite, deve ser
+# ~ informado que ele excedeu o número de transações permitidas para aquele dia.
+# ~ Mostrar no extrato a data e hora de todas as transações.
+
 MENU = """
 |''''''''''''''''''''''''''''''''''''|
 | [1] DEPOSITAR                      |
@@ -29,20 +41,30 @@ MENU = """
 |,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,|
 """
 
-LIMITE_SAQUE_AO_DIA = 3
-LIMITE_POR_SAQUE = 500.00
+LIMITE_SAQUE_DIARIO = 3
+LIMITE_SAQUE = 500.00
+LIMITE_TRANSACOES_DIARIAS = 10
 
 saldo = 0.0
 qtde_saque_ao_dia = 0
-extrato = "Extrato:"
+qtde_transacoes_diaria = 0
+extrato = "Extrato:\n"
 
 while True:
+    data_atual = datetime.now()
+
     print(MENU)
     escolha = input("Digite uma das opções: ")
     print("-------------------------")
     if escolha not in ["0", "1", "2", "3"]:
         print("OPÇÃO NÃO ENCONTRADA. TENTE NOVAMENTE.")
         continue
+
+    if escolha in ["1", "2"]:
+        qtde_transacoes_diaria += 1
+        if qtde_transacoes_diaria > LIMITE_TRANSACOES_DIARIAS:
+            print("VOCÊ ATINGIU O LIMITE DE TRANSAÇÕES PERMITIDAS AO DIA.")
+            continue
 
     if escolha == "1":
         try:
@@ -56,11 +78,13 @@ while True:
             continue
 
         saldo += deposito
-        extrato += f"\n   Depositado   ->    R$ {deposito:+5,.2f}"
+        extrato += f"\n   Depositado   ->    R$ {deposito:+5,.2f}         [{
+            data_atual.strftime('%d-%m-%y %H:%M')
+        }]"
         continue
 
     if escolha == "2":
-        if qtde_saque_ao_dia >= LIMITE_SAQUE_AO_DIA:
+        if qtde_saque_ao_dia >= LIMITE_SAQUE_DIARIO:
             print("NÚMERO DE SAQUES DIÁRIOS0 EXCEDIDO.")
             continue
 
@@ -70,7 +94,7 @@ while True:
             print("VALOR INVÁLIDO.")
             continue
 
-        if saque > LIMITE_POR_SAQUE:
+        if saque > LIMITE_SAQUE:
             print("LIMITE POR SAQUE EXCEDIDO. MAX: R$ 500.")
             continue
 
@@ -78,13 +102,21 @@ while True:
             print("NÃO É POSSÍVEL SACAR VALORES NEGATIVOS.")
             continue
 
+        if saque > saldo:
+            print("SALDO INSUFICIENTE.")
+            continue
+
         saldo -= saque
-        extrato += f"\n   Saque   ->    R$ {saque:+5,.2f}"
+        extrato += f"\n   Saque        ->    R$ {saque:+5,.2f}         [{
+            data_atual.strftime('%d-%m-%y %H:%M')
+        }]"
         qtde_saque_ao_dia += 1
         continue
 
     if escolha == "3":
-        str_saldo_atual = f"\n   Saldo atual:       R$ {saldo:+5,.2f}"
+        str_saldo_atual = f"\n   Saldo atual:       R$ {saldo:+5,.2f}         [{
+            data_atual.strftime('%d-%m-%y %H:%M')
+        }]"
         print(extrato)
         print(str_saldo_atual)
         continue
@@ -92,3 +124,5 @@ while True:
     if escolha == "0":
         print("FECHANDO APLICATIVO BANCÁRIO...")
         break
+
+# info = {data_atual.strftime("%d-%m-%y"): qtde_transacoes_diaria}
